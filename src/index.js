2bb52cdff3fa79a36dd7fa76d03b78e1fb9382ec
Jason Colina <rickjason.colina@gmail.com>
2020-04-10 19:00:07 -0400

    added an input and button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Create a react component
 
+// Create a react component
+const App = () => {
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{ backgroundColor:'blue', color:'white', borderRadius:'5px', padding:'5px' }}>Submit</button>
+        </div>
+    );
+};
 
 // Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
