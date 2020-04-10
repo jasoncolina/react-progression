5ec04188d467927566946dfa98a54f070a76f826
Jason Colina <rickjason.colina@gmail.com>
2020-04-10 18:35:56 -0400

    created index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Create a react component
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Take the react component and show it on the screen
