5f5e5a1324190334172b15ccc980d007e6cb033c
Jason Colina <rickjason.colina@gmail.com>
2020-04-14 15:09:18 -0400

    added cdn

--- src/index.js
+++ src/index.js
@@ -22,3 +22,5 @@ ReactDOM.render(
     <App />,
     document.querySelector('#root')
 );
+
+//<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
